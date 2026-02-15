4d73031a85dde738ce18e6c75212668cb3251a5f
ViniciusCesar41ls <vinicius.guimaraes.cesar.41@outlook.com>
2026-02-14 21:03:20 -0300

    Two functions

--- main.rs
+++ main.rs
@@ -1,3 +1,17 @@
 fn main (){
     println!("Hello world!");
+
+    let mut x: i32 = 5;
+
+    x += 1;
+
+    assert_eq!(x, 6);
+    println!("sucess!");
+
+    define_y();
+}
+
+fn define_y(){
+    let y: &str = "hello";
+    println!("{} world!", y);
 }
