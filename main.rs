cff28a525f0dfe750e9cc45d667803ab38924486
ViniciusCesar41ls <vinicius.guimaraes.cesar.41@outlook.com>
2026-02-14 21:29:16 -0300

    Tuple

--- main.rs
+++ main.rs
@@ -1,3 +1,5 @@
+// #[allow(unused_variables)]
+
 fn main (){
     println!("Hello world!");
 
@@ -9,9 +11,23 @@ fn main (){
     println!("sucess!");
 
     define_y();
+
+    // let x = 1;
+
+    tuple_test();
 }
 
 fn define_y(){
     let y: &str = "hello";
     println!("{} world!", y);
+}
+
+fn tuple_test(){
+    let (mut x, y) = (1,2);
+    x += 2;
+
+    assert_eq!(x,3);
+    assert_eq!(y,2);
+    println!("tuplesuccess!");
+    
 }
